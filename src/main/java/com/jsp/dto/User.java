d44fd270c593fb21f2e16b78a646feb958a21e87
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2023-12-28 15:58:51 +0900

    Initial commit

---
+++ src/main/java/com/jsp/dto/User.java
@@ -0,0 +1,60 @@
+package com.jsp.dto;
+
+public class User {
+
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userNickName;
+	private String userEmail;
+	private String userResistNum;
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserNickName() {
+		return userNickName;
+	}
+
+	public void setUserNickName(String userNickName) {
+		this.userNickName = userNickName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserResistNum() {
+		return userResistNum;
+	}
+
+	public void setUserResistNum(String userResistNum) {
+		this.userResistNum = userResistNum;
+	}
+
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="">
+		<input type="text" placeholder="ID를 입력해주세요" /> <input type="password"
+			placeholder="password를 입력해주세요" /> <input type="submit" />
+	</form>
+</body>
+</html>
